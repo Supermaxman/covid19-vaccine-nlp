4fa8b4057d1eea465dfeb7c9363ff87f682b0cc1
Max Weinzierl <maxwellweinzierl@gmail.com>
2021-09-16 12:38:14 -0500

    Initial commit

--- multi_class/predict.py
+++ multi_class/predict.py
@@ -1,7 +1,7 @@
 
 from pytorch_lightning.utilities.cli import LightningCLI
 
-from pytorch_gleam.data.datasets.misinfo_stance import MultiClassMisinfoDataModule
+from pytorch_gleam.data.datasets import MultiClassMisinfoStanceDataModule
 from pytorch_gleam.modeling.models.multi_class import MultiClassLanguageModel
 from pytorch_gleam.callbacks.prediction import JsonlWriter
 from pytorch_gleam.callbacks.checkpoint import FitCheckpointCallback
@@ -10,7 +10,7 @@ from pytorch_gleam.callbacks.checkpoint import FitCheckpointCallback
 if __name__ == '__main__':
 	cli = LightningCLI(
 		MultiClassLanguageModel,
-		MultiClassMisinfoDataModule,
+		MultiClassMisinfoStanceDataModule,
 		run=False,
 		trainer_defaults={
 			'checkpoint_callback': False,
