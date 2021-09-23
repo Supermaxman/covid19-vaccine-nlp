73902022e2837b43be3cdcc6989a673c92ba1b91
Max Weinzierl <maxwellweinzierl@gmail.com>
2021-09-23 15:03:25 -0500

    Initial commit

--- experiments/kbi/kbi.sh
+++ experiments/kbi/kbi.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 config=experiments/kbi/kbi-v53.yaml
-run_id=${config::-5}
-
-seed=0
+config_name=`basename ${config}`
+run_id=${config_name::-5}
 num_gpus=1
 
 
@@ -13,7 +12,7 @@ echo "Starting experiment ${run_id}"
 echo "Reserving ${num_gpus} GPU(s)..."
 
 gpus=`request-gpus -r ${num_gpus}`
-if [[ ${gpus} -eq -1 ]]; then
+if [[ ${gpus} == "-1" ]]; then
     echo "Unable to reserve ${num_gpus} GPU(s), exiting."
     exit -1
 fi
@@ -32,7 +31,6 @@ trap handler SIGINT
 echo "Training ${run_id} model..."
 python kbi/train.py \
   --config ${config} \
-  --seed_everything ${seed} \
   --trainer.gpus ${gpus} \
   --trainer.default_root_dir models/${run_id} \
 
--- kbi/train.py
+++ kbi/train.py
@@ -3,9 +3,6 @@ from pytorch_lightning.utilities.cli import LightningCLI
 
 from pytorch_gleam.data.datasets import KbiMisinfoStanceDataModule
 from pytorch_gleam.modeling.models import KbiLanguageModel
-from pytorch_gleam.callbacks import FitCheckpointCallback
-from pytorch_gleam.callbacks import CopyConfigCallback
-
 
 if __name__ == '__main__':
 	cli = LightningCLI(
@@ -13,14 +10,7 @@ if __name__ == '__main__':
 		KbiMisinfoStanceDataModule,
 		run=False,
 		subclass_mode_model=True,
-		subclass_mode_data=True,
-		trainer_defaults={
-			'checkpoint_callback': False,
-			'callbacks': [
-				FitCheckpointCallback(),
-				CopyConfigCallback()
-			]
-		}
+		subclass_mode_data=True
 	)
 	cli.trainer.fit(
 		cli.model,
