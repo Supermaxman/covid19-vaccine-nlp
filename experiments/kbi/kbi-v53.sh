590de2d06ce3e279ea7587247b82923069c2faa4
Max Weinzierl <maxwellweinzierl@gmail.com>
2021-09-23 13:09:04 -0500

    Initial commit

--- experiments/kbi/kbi-v53.sh
+++ experiments/kbi/kbi-v53.sh
@@ -1,23 +1,13 @@
 #!/usr/bin/env bash
 
-filename=$(basename -- "$0")
-run_id=${filename::-3}
+config=experiments/kbi/kbi-v53.yaml
+run_id=${config::-5}
 
 seed=0
 num_gpus=1
-config=experiments/kbi/${run_id}.yaml
-
-data_path=/users/max/code/covid19-vaccine-twitter/data/v1
-train_path=${data_path}/train.jsonl
-train_misinfo_path=${data_path}/misinfo.json
-val_path=${data_path}/dev.jsonl
-val_misinfo_path=${data_path}/misinfo.json
-test_path=${data_path}/test.jsonl
-test_misinfo_path=${data_path}/misinfo.json
 
 
 export TOKENIZERS_PARALLELISM=true
-
 
 echo "Starting experiment ${run_id}"
 echo "Reserving ${num_gpus} GPU(s)..."
@@ -45,10 +35,6 @@ python kbi/train.py \
   --seed_everything ${seed} \
   --trainer.gpus ${gpus} \
   --trainer.default_root_dir models/${run_id} \
-  --data.init_args.train_path ${train_path} \
-  --data.init_args.train_misinfo_path ${train_misinfo_path} \
-  --data.init_args.val_path ${val_path} \
-  --data.init_args.val_misinfo_path ${val_misinfo_path}
 
 
 echo "Freeing ${num_gpus} GPUs: ${gpus}"
