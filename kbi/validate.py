58474f691aa9a78054bf990b29c78bee2f776bd0
Max Weinzierl <maxwellweinzierl@gmail.com>
2021-09-28 15:41:25 -0500

    New experiments for covaxlies

--- kbi/validate.py
+++ kbi/validate.py
@@ -3,20 +3,14 @@ from pytorch_lightning.utilities.cli import LightningCLI
 
 from pytorch_gleam.data.datasets import KbiMisinfoStanceDataModule
 from pytorch_gleam.modeling.models import KbiLanguageModel
-from pytorch_gleam.callbacks import FitCheckpointCallback
-
 
 if __name__ == '__main__':
 	cli = LightningCLI(
 		KbiLanguageModel,
 		KbiMisinfoStanceDataModule,
 		run=False,
-		trainer_defaults={
-			'checkpoint_callback': False,
-			'callbacks': [
-				FitCheckpointCallback()
-			]
-		}
+		subclass_mode_model=True,
+		subclass_mode_data=True
 	)
 	cli.trainer.validate(
 		cli.model,
