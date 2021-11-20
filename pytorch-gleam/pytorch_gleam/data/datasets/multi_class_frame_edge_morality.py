5f7f01a339eb5d8e71897f76b6b3688faa18003a
Max Weinzierl <maxwellweinzierl@gmail.com>
2021-11-20 14:25:50 -0600

    Added multi class frame graph experiments

--- pytorch-gleam/pytorch_gleam/data/datasets/multi_class_frame_edge_morality.py
+++ pytorch-gleam/pytorch_gleam/data/datasets/multi_class_frame_edge_morality.py
@@ -176,24 +176,19 @@ def create_edges(
 	semantic_adj = np.eye(seq_len, dtype=np.float32)
 	emotion_adj = np.eye(seq_len, dtype=np.float32)
 	lexical_adj = np.eye(seq_len, dtype=np.float32)
-
-	for input_idx in range(seq_len):
-		input_idx_text = t_map[input_idx]
-		if input_idx_text in semantic_edges:
-			for e_txt in semantic_edges[input_idx_text]:
-				for edge_idx in r_map[e_txt]:
-					semantic_adj[input_idx, edge_idx] = 1.0
-					semantic_adj[edge_idx, input_idx] = 1.0
-		if input_idx_text in emotion_edges:
-			for e_txt in emotion_edges[input_idx_text]:
-				for edge_idx in r_map[e_txt]:
-					emotion_adj[input_idx, edge_idx] = 1.0
-					emotion_adj[edge_idx, input_idx] = 1.0
-		if input_idx_text in lexical_edges:
-			for e_txt in lexical_edges[input_idx_text]:
-				for edge_idx in r_map[e_txt]:
-					lexical_adj[input_idx, edge_idx] = 1.0
-					lexical_adj[edge_idx, input_idx] = 1.0
+	for input_idx_text, input_indices in r_map.items():
+		for e_txt in semantic_edges[input_idx_text]:
+			r_indices = r_map[e_txt]
+			semantic_adj[input_indices, r_indices] = 1.0
+			semantic_adj[r_indices, input_indices] = 1.0
+		for e_txt in emotion_edges[input_idx_text]:
+			r_indices = r_map[e_txt]
+			emotion_adj[input_indices, r_indices] = 1.0
+			emotion_adj[r_indices, input_indices] = 1.0
+		for e_txt in lexical_edges[input_idx_text]:
+			r_indices = r_map[e_txt]
+			lexical_adj[input_indices, r_indices] = 1.0
+			lexical_adj[r_indices, input_indices] = 1.0
 
 	print(f'create_adjacency_matrix {time.time()-start:.4f} seconds')
 	edges = {
