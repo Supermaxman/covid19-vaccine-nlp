d30b61b023212221fe1d6ea81af4f01b403438d9
Max Weinzierl <maxwellweinzierl@gmail.com>
2021-11-20 13:11:17 -0600

    Added multi class frame graph experiments

--- pytorch-gleam/pytorch_gleam/data/datasets/__init__.py
+++ pytorch-gleam/pytorch_gleam/data/datasets/__init__.py
@@ -5,4 +5,5 @@ from pytorch_gleam.data.datasets.nli_text_stance import NliTextMisinfoStanceData
 from pytorch_gleam.data.datasets.multi_label import MultiLabelDataModule
 from pytorch_gleam.data.datasets.multi_class_frame import MultiClassFrameDataModule
 from pytorch_gleam.data.datasets.multi_class_frame_edge import MultiClassFrameEdgeDataModule
+from pytorch_gleam.data.datasets.multi_class_frame_edge_morality import MultiClassFrameEdgeMoralityDataModule
 from pytorch_gleam.data.datasets.base_datasets import BaseDataModule
