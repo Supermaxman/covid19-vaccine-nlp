dc41d585e65b44a688c20d05d60173d28a75231d
Max Weinzierl <maxwellweinzierl@gmail.com>
2021-12-31 12:15:37 -0600

    `Efficient and correct iterable prediction code

---
+++ experiments/profile/cluster-v2-k5.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+data_path=/nas1-nfs1/data/maw150130/covid19
+base_name=covid19-frame-rel-v2_stance
+profile_type=sign
+cluster_version=v2
+cluster_count=5
+
+python pytorch-gleam/pytorch_gleam/stance/stance_profile.py \
+  --input_path ${data_path}/${base_name}-scores.json \
+  --frame_map_path ${data_path}/frame_map.json \
+  --output_path ${data_path}/${base_name}-profiles-m${profile_type}.pk \
+  --mode sign \
+  --num_processes 12
+
+python pytorch-gleam/pytorch_gleam/stance/profile_cluster.py \
+  --input_path ${data_path}/${base_name}-profiles-m${profile_type}.pk \
+  --output_path ${data_path}/${base_name}-clusters-m${profile_type}-v${cluster_version}-k${cluster_count}.json \
+  --num_clusters ${cluster_count}
+
+python pytorch-gleam/pytorch_gleam/stance/analyze_cluster.py \
+  --input_path ${data_path}/${base_name}-clusters-m${profile_type}-v${cluster_version}-k${cluster_count}.json  \
+  --user_path ${data_path}/${base_name}-profiles-m${profile_type}.pk \
+  --theme_path ${data_path}/theme_map.json
+
+
+
+
