7c220fbb6d9a40401bb7ea77509fa7f4b093de8d
Max Weinzierl <maxwellweinzierl@gmail.com>
2021-12-31 11:36:14 -0600

    `Efficient and correct iterable prediction code

--- pytorch-gleam/pytorch_gleam/stance/analyze_cluster.py
+++ pytorch-gleam/pytorch_gleam/stance/analyze_cluster.py
@@ -70,7 +70,7 @@ def main():
 	for cluster_id, cluster in sorted(clusters.items(), key=lambda x: len(x[1]['users'])):
 		c_users = cluster['users']
 		c_centroid = cluster['centroid']
-		print(f'{cluster_id}: {len(c_users):,} users; {100*len(c_users)/total_users:.0f}%')
+		print(f'cluster {cluster_id}: {len(c_users):,} users ({100*len(c_users)/total_users:.0f}%)')
 		current_tax = None
 		for t_idx, t_text in idx2txt.items():
 			t_score = c_centroid[t_idx]
